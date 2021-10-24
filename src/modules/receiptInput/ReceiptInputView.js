1d96e64a294ebf150895522662a57ecc7e1e7492
Rexter Delos Santos <rexterds@hawaii.edu>
2021-10-23 19:27:18 -1000

    added some styling

--- src/modules/receiptInput/ReceiptInputView.js
+++ src/modules/receiptInput/ReceiptInputView.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Text, View, TextInput, Button,  StyleSheet } from "react-native";
+import { Text, View, ScrollView, TextInput, Button,  StyleSheet } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function ReceiptInputScreen() {
 
-  const { handleSubmit, control, reset, formState: { errors } } = useForm({
+  const { handleSubmit, control, reset, getValues, formState: { errors } } = useForm({
     defaultValues: {
       companyName: "",
       date: new Date(),
@@ -22,64 +22,121 @@ export default function ReceiptInputScreen() {
   console.log('errors', errors);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Company Name</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={input => onChange(input)}
-            value={value}
+    <ScrollView style={styles.container}>
+      <View>
+        <Text style={styles.label}>Company Name</Text>
+        <Controller
+          control={control}
+          render={({field: { onChange, onBlur, value }}) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={input => onChange(input)}
+              value={value}
+            />
+          )}
+          name="companyName"
+          rules={{ required: true }}
+        />
+      </View>
+      <View style={styles.row}>
+        <View style={styles.half}>
+          <Text style={styles.label}>Date</Text>
+          <Button onPress={() => setShow(!show)} title={`${getValues("date")}`} />
+          <Controller
+            control={control}
+            render={({field: { onChange, value }}) => (
+              show && (
+              <DateTimePicker 
+                value={value}
+                mode="date"
+                onChange={(event, newDate) => {
+                  setShow(!show);
+                  onChange(newDate);
+                }}
+              />
+    )
+            )}
+            name="date"
+            rules={{ required: true }}
           />
-        )}
-        name="companyName"
-        rules={{ required: true }}
-      />
-      <Text style={styles.label}>Date</Text>
-      <Button onPress={() => setShow(!show)} title="Choose date" />
-      <Controller
-        control={control}
-        render={({field: { onChange, value }}) => (
-          show && (
-          <DateTimePicker 
-            value={value}
-            mode="date"
-            onChange={(event, newDate) => {
-              setShow(!show);
-              onChange(newDate);
-            }}
+        </View>
+        <View style={styles.half}>
+          <Text style={styles.label}>Total Expenses</Text>
+          <Controller
+            control={control}
+            render={({field: { onChange, onBlur, value }}) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={input => onChange(input)}
+                value={value}
+              />
+          )}
+            name="totalExpenses"
+            rules={{ required: true }}
           />
-)
-        )}
-        name="date"
-        rules={{ required: true }}
-      />
-      <Text style={styles.label}>Description</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={input => onChange(input)}
-            value={value}
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.half}>
+          <Text style={styles.label}>Keywords</Text>
+          <Controller
+            control={control}
+            render={({field: { onChange, onBlur, value }}) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={input => onChange(input)}
+                value={value}
+              />
+          )}
+            name="keywords"
+            rules={{ required: true }}
           />
-        )}
-        name="description"
-        rules={{ required: true }}
-      />
-
+        </View>
+        <View style={styles.half}>
+          <Text style={styles.label}>Category</Text>
+          <Controller
+            control={control}
+            render={({field: { onChange, onBlur, value }}) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={input => onChange(input)}
+                value={value}
+              />
+          )}
+            name="category"
+            rules={{ required: true }}
+          />
+        </View>
+      </View>
+      <View>
+        <Text style={styles.label}>Description</Text>
+        <Controller
+          control={control}
+          render={({field: { onChange, onBlur, value }}) => (
+            <TextInput
+              style={styles.description}
+              onBlur={onBlur}
+              onChangeText={input => onChange(input)}
+              value={value}
+            />
+          )}
+          name="description"
+          rules={{ required: true }}
+        />
+      </View>
       <View style={styles.button}>
         <Button
           style={styles.buttonInner}
           color
-          title="Button"
+          title="Submit"
           onPress={handleSubmit(onSubmit)}
         />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -88,6 +145,12 @@ const styles = StyleSheet.create({
     color: 'white',
     margin: 20,
     marginLeft: 0,
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  half: {
+    flex: 1,
   },
   button: {
     marginTop: 40,
@@ -98,10 +161,15 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
     paddingTop: 10,
-    padding: 8,
+    padding: 10,
     backgroundColor: '#0e101c',
+  },
+  description: {
+    backgroundColor: 'white',
+    height: 100,
+    padding: 10,
+    borderRadius: 4,
   },
   input: {
     backgroundColor: 'white',
